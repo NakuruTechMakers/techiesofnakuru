ce708221022dc1db96728a2d5549ed91911c25b7
Isale <isale.bryan@gmail.com>
2021-01-23 12:13:23 +0300

    add collapsible JS code

--- js/main.js
+++ js/main.js
@@ -468,6 +468,21 @@
 
     })();
         
+    // Collapsible Buttons 
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight){
+        content.style.maxHeight = null;
+        } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+    }
         
 })(jQuery);
 
